a63348890930a0f45bb3ebf5ec05e50595da7c06
juangiraldor <juanesgiraldo94@gmail.com>
2026-03-05 22:18:16 -0500

    Adding products fixture and commands to add to cart, adding command to fill the form anf complete checkout

--- Automatizacion-E2E/cypress/e2e/prueba-e2e.cy.js
+++ Automatizacion-E2E/cypress/e2e/prueba-e2e.cy.js
@@ -1,9 +1,34 @@
 describe('Prueba E2E: Flujo completo de compra', () => {
-    it ('Login correcto', () => {
+    it ('Flujo de compra correcto con 2 productos', () => {
+        //Realizar login
         cy.visit('/')
         cy.fixture('users').then((data) => {
             cy.login(data.standard.username, data.standard.password)
         })
         cy.get("[data-test=title]").should('be.visible')
+        
+        //Agregar 2 productos al carrito
+        cy.fixture('products').then((data) => {
+            const productsToAdd = [
+                data.BikeLight.dataTest,
+                data.Jacket.dataTest
+            ]
+            cy.addProducts(productsToAdd)
+        })
+
+        //Visualizar el carrito
+        cy.get("[data-test=shopping-cart-link]").click()
+        cy.fixture('products').then((data) => {
+            cy.contains(data.BikeLight.name)
+            cy.contains(data.Jacket.name)
+        })
+
+        //Completar formulario de compra
+        cy.fixture('users').then((data) => {
+            cy.checkout(data.standard.firstName, data.standard.lastName, data.standard.postalCode)
+        })
+
+        //Confirmación de compra
+        cy.get("[data-test=complete-header]").should('be.visible')
     })
 })
--- Automatizacion-E2E/cypress/support/commands.js
+++ Automatizacion-E2E/cypress/support/commands.js
@@ -3,3 +3,18 @@ Cypress.Commands.add('login', (user, password) => {
     cy.get("[data-test=password]").clear().type(password)
     cy.get("[data-test=login-button]").click()
 })
+
+Cypress.Commands.add('addProducts', (products) => {
+    products.forEach((product) => {
+        cy.get(`[data-test=${product}]`).click()
+    })  
+})
+
+Cypress.Commands.add('checkout', (firstName, lastName, postalCode) => {
+    cy.get("[data-test=checkout]").click()
+    cy.get("[data-test=firstName]").clear().type(firstName)
+    cy.get("[data-test=lastName]").clear().type(lastName)
+    cy.get("[data-test=postalCode]").clear().type(postalCode)
+    cy.get("[data-test=continue]").click()
+    cy.get("[data-test=finish]").click()
+})
